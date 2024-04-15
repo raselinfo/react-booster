f8e98fd1a2bee8c9742093f6e922c0a24f8b17db
Rasel Hossain <rasel@stacklearner.com>
2024-04-15 22:46:11 +0600

    add edge function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
+
+// export const dynamic= "force-dynamic"
+export const runtime="edge"
+
+
 const images = [
   "https://utfs.io/f/134fba57-0995-4ff0-99fd-2c7ec3ce51cc-8ckntv.png",
   "https://utfs.io/f/f1f0f9e3-111d-4c70-9ad8-f5b9bd6dbe2c-h64va5.jpg",

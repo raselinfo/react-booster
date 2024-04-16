b8d70538904d8318468aad1f837c459c4a822a78
Rasel Hossain <rasel@stacklearner.com>
2024-04-16 19:08:41 +0600

    add shadcdn

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,9 @@
+export const runtime = "edge";
+
+export default async function HomePage() {
+  return (
+    <main className="flex flex-wrap gap-2">
+      <h1>Hello world</h1>
+    </main>
+  );
+}

264fb07b1964056fe160a8c3a918e92b166887fe
Rasel Hossain <rasel@stacklearner.com>
2024-04-16 19:19:47 +0600

    add shadcn button

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "~/components/ui/button";
+
 export const runtime = "edge";
 
 export default async function HomePage() {
   return (
     <main className="flex flex-wrap gap-2">
       <h1>Hello world</h1>
+      <Button>Click Me</Button>
     </main>
   );
 }
